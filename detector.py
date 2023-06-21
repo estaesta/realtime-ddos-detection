95cce844f4c3445b5d1f81b5d27a982d93593f40
estaesta <ahmadforesta@gmail.com>
2023-06-21 13:09:37 +0700

    feat: detector.py

---
+++ detector.py
@@ -0,0 +1,40 @@
+import subprocess
+import requests
+import signal
+from threading import Thread
+import sys
+
+def send_file(filename, url):
+    with open(filename, 'rb') as file:
+        response = requests.post(url, files={'file': file})
+        print(response.text)
+        return response.text
+
+def run_script():
+    api_url = sys.argv[1]
+    flag = True
+    while flag:
+        # Run the command
+        command = f"sudo env/bin/cicflowmeter -i enp0s8 -c -d 10  output.csv"
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+        process.wait()
+
+        # Start a thread for the POST request
+        print('sending to url')
+        thread = Thread(target=send_file, args=('output.csv', api_url))
+        thread.start()
+
+        #repeat for output2
+        command = f"sudo env/bin/cicflowmeter -i enp0s8 -c -d 10 output2.csv"
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+        process.wait()
+
+        # Start a thread for the POST request
+        print('sending to url')
+        thread = Thread(target=send_file, args=('output2.csv', api_url))
+        thread.start()
+
+if __name__ == '__main__':
+    run_script()
