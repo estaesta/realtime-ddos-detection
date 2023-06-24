8ac80075d24f9a1570b6093de32e356c53b21327
estaesta <ahmadforesta@gmail.com>
2023-06-24 12:02:44 +0700

    fix(mac): remove mac

--- src/cicflowmeter/features/context/packet_flow_key.py
+++ src/cicflowmeter/features/context/packet_flow_key.py
@@ -34,15 +34,11 @@ def get_packet_flow_key(packet, direction) -> tuple:
     if direction == PacketDirection.FORWARD:
         dest_ip = packet["IP"].dst
         src_ip = packet["IP"].src
-        dest_mac = packet["Ether"].dst
-        src_mac = packet["Ether"].src
         src_port = packet[protocol].sport
         dest_port = packet[protocol].dport
     else:
         dest_ip = packet["IP"].src
         src_ip = packet["IP"].dst
-        dest_mac = packet["Ether"].src
-        src_mac = packet["Ether"].dst
         src_port = packet[protocol].dport
         dest_port = packet[protocol].sport
 
